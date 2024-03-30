4450012a5addf41dc8e0d8e1853b68b3f8218ad6
Jamie Anderson <JamieThomasAnderson@protonmail.com>
2024-03-30 22:49:02 +1000

    write and output instruction

--- include/chunk.h
+++ include/chunk.h
@@ -3,12 +3,20 @@
 
 #include "common.h"
 
-typedef enum {
-  OP_RETURN,
+typedef enum
+{
+    OP_RETURN,
 } Opcode;
 
-typedef struct {
-  uint8_t* code;
+typedef struct
+{
+    int count;
+    int capacity;
+    uint8_t *code;
 } Chunk;
+
+void initChunk(Chunk *chunk);
+void freeChunk(Chunk *chunk);
+void writeChunk(Chunk *chunk, uint8_t byte);
 
 #endif
--- main.c
+++
@@ -1,6 +0,0 @@
-
-#include "include/common.h"
-
-int main(int arc, const char* argv[]){
-    return 0;
-}
